49cb94cd0f2f0d1693e2e59d52b2d8472031d63a
Marek Prochazka <m.prochazka2002@gmail.com>
2020-09-15 19:02:07 +0200

    #11 - Refactoring(deleted useless imports in app.py)

--- app.py
+++ app.py
@@ -1,38 +1,24 @@
 from tkinter import *
-import tkinter.ttk as t
-from tkinter.ttk import Button
-import tkinter.colorchooser as col
-from colormap import rgb2hex
-from math import floor
-from tkinter import filedialog
-from PIL import Image
-import json
-from numpy import sin, cos, tan, pi
 import matplotlib as mp
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import matplotlib.animation as anim
 from matplotlib import style as st
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 import webbrowser
-# Ver. Alpha 1.6
-#
 
 
-from Static.constants import *
-from Globals.calculated import *
-from Globals.variables import Variables as V
+
+
 from GUI.base import Base
 from Utils import DeleteAll, Saving, ShowFrame, UpdateTable, Grid, graph_update
 from Graphing.graph_animation import GraphAnimation
 from Console.console import Console
+
 mp.use("TkAgg")
 with open("graphstyle.txt", "r") as style:
     st.use(style)
 
 # st.use('ggplot')
 
-import numpy as np
+
 
 # f = plt.figure(figsize=(4.5, 4.5), dpi=100)
 from Graphing.setup import *
@@ -57,7 +43,6 @@ class MarkoGebra(Base,DeleteAll,Saving,ShowFrame,UpdateTable,Grid,graph_update.M
 
     def openHelp(self):
         webbrowser.open(url="https://gist.github.com/RandomResourceWeb/93e887facdb98937ab5d260d1a0df270", new=1)
-        webbrowser.open(url="D:\Věci\Programování\Dlohodoba_prace_main_2020\web\index.html", new=1)
 
 
 
