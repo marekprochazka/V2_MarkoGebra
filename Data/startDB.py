5d26dd51fbe70ee5ffeec4645d9cee29ded644a2
Marek Prochazka <m.prochazka2002@gmail.com>
2020-09-28 18:14:43 +0200

    #1 - (value field in bar table changed to INTEGER)

--- Data/startDB.py
+++ Data/startDB.py
@@ -6,7 +6,7 @@ c = conn.cursor()
 
 c.execute("""CREATE TABLE scatter (id TEXT PRIMARY KEY ,x INTEGER ,y INTEGER ,marker TEXT,color TEXT, size REAL)""")
 c.execute("CREATE TABLE function (id TEXT PRIMARY KEY ,func TEXT,line TEXT,color TEXT,size REAL)")
-c.execute("CREATE TABLE bar (id TEXT PRIMARY KEY ,name TEXT, value TEXT, color TEXT, width REAL)")
+c.execute("CREATE TABLE bar (id TEXT PRIMARY KEY ,name TEXT, value INTEGER, color TEXT, width REAL)")
 c.execute("CREATE TABLE pie (id TEXT PRIMARY KEY ,slice REAL,activity TEXT,color TEXT,explode REAL)")
 #TODO finish random noise
 
