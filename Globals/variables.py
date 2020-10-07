aea8c313336e6de9cb351301f9cd1af58c3b9511
Marek Prochazka <m.prochazka2002@gmail.com>
2020-10-07 21:26:51 +0200

    #11 - (deleted useless variables)

--- Globals/variables.py
+++ Globals/variables.py
@@ -4,34 +4,13 @@ from Static.constants import X, Y, MIN, MAX
 class Variables:
     to_animate = None
 
-    pie_colors = []
-
     command_history = []
     history_moves = 0
 
     cache = []
     changes_cache = []
 
-    coordinates_scatter = []
-    coordinates_plot = []
-    coordinates_all_list = []
-
-    slices = []
-    cols = []
-    activities = []
-    explode = []
-    start_angle = 90
-
-    bars = []
-    # [name,value,color]
-
-    noises = [[[0, 0, ".", "#fff", 1], [0, 0, ".", "#fff", 1]]]
-    dispersion = []
-    number = []
-    basic_gen = []
-
-    lim1 = 30
-    lim2 = -30
+    # noises = [[[0, 0, ".", "#fff", 1], [0, 0, ".", "#fff", 1]]]
 
     limits = {
         X: {
