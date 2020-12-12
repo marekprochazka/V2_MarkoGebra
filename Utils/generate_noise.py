cd18090f394a42812028536c6924a4b61a48c682
Marek Prochazka <m.prochazka2002@gmail.com>
2020-12-12 15:23:48 +0100

    #65 - generate noise

---
+++ Utils/generate_noise.py
@@ -0,0 +1,12 @@
+from Utils.generate_seed import generate_seed
+from random import randint, seed
+import numpy as np
+
+
+def generate_noise(dispersion:int, quantity:int ):
+    r_seed = generate_seed()
+    seed(r_seed)
+    generation = [[randint(-dispersion,dispersion),randint(-dispersion,dispersion)] for _ in range(quantity)]
+    return (np.array(generation),r_seed)
+
+print(generate_noise(50,150))
---
+++ Utils/generate_seed.py
@@ -0,0 +1,3 @@
+import os
+def generate_seed():
+    return os.urandom(8)
