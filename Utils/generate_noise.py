c8e635a84ab0e1e3f7ae7addc3c120d873b5e96e
Marek Prochazka <m.prochazka2002@gmail.com>
2020-12-13 10:34:41 +0100

    #34 - (generator upgrade: working with existing seed)

--- Utils/generate_noise.py
+++ Utils/generate_noise.py
@@ -1,16 +1,17 @@
 from Utils.generate_seed import generate_seed
 import numpy as np
 
-def generate_new_noise(dispersion: float, quantity: int):
-    r_seed = generate_seed()
+def generate_noise(dispersion: float, quantity: int, seed=None):
+    if not seed:
+        r_seed = generate_seed()
+    else: r_seed = seed
     np.random.seed(r_seed)
     generation = np.random.random((quantity, 2))
     generation = np.floor(((generation * 2) - 1) * dispersion)
     return generation, r_seed
 
 
-def generate_with_seed():
-    pass
 
 
-print(generate_new_noise(50, 153))
+
+# print(generate_noise(50, 153))
