4634e0068e73b74056265580fcceb9ab3481db99
Marek Prochazka <m.prochazka2002@gmail.com>
2020-12-19 09:02:58 +0100

    feat - (live noise is always on top)

--- Graphing/graph_animation.py
+++ Graphing/graph_animation.py
@@ -73,10 +73,10 @@ class GraphAnimation:
     def animate_noise(self):
         a.clear()
         a.axis("equal")
-        if V.live_noise:
-            a.scatter(V.live_noise[0][:,0],V.live_noise[0][:,1],color=V.live_noise[1], marker=V.live_noise[2])
         for val in V.cache[0]:
             a.scatter(val[-1][:,0],val[-1][:,1],color=val[4], marker=val[5])
+        if V.live_noise:
+            a.scatter(V.live_noise[0][:, 0], V.live_noise[0][:, 1], color=V.live_noise[1], marker=V.live_noise[2])
 
     def __zero_x(self, fun):
         try:
