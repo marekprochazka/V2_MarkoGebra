ab4f72d98c59bd934aea0dd62b830fac6c9cea59
Marek Prochazka <m.prochazka2002@gmail.com>
2021-03-10 09:47:27 +0100

    #104 - fix (zero values in scatter input)

--- Decorators/input_checkers/scatter.py
+++ Decorators/input_checkers/scatter.py
@@ -5,11 +5,11 @@ def scatter_input_controller(func):
     def wrapper(*args,**kwargs):
         data = func(*args, **kwargs)
         cache = data[CACHE]
-        if not cache[1]:
+        if cache[1] is None:
             data[ERRORS].append({NAME: "input error", INFO:"Hodnota 'x' musí být vyplněna"})
-        if not cache[2]:
+        if cache[2] is None:
             data[ERRORS].append({NAME: "input error", INFO:"Hodnota 'y' musí být vyplněna"})
-        if not cache[5]:
+        if cache[5] is None:
             data[ERRORS].append({NAME: "input error", INFO:"Hodnota 'velikost' musí být vyplněna"})
         return data
     return wrapper
