836599fec145ef5d5381c2e87c9e12f349956588
Marek Prochazka <m.prochazka2002@gmail.com>
2021-03-12 10:14:12 +0100

    #103 - (noise gui input position swap)

--- GUI/pages/noise.py
+++ GUI/pages/noise.py
@@ -31,30 +31,30 @@ class Noise(Frame):
         # GUI DEFINITION/GRIDING
         self.generate_button = t.Button(self, text="Generuj",
                                         command=self.__create_live_generation)
-        self.generate_button.grid(row=0, column=0, sticky="nswe")
+        self.generate_button.grid(row=3, column=0, sticky="nswe")
         self.lock = t.Button(self, text="Uzamknout",
                              command=self.__update_noise_data)
-        self.lock.grid(row=0, column=1, sticky="nswe")
+        self.lock.grid(row=3, column=1, sticky="nswe")
 
         self.quantity_label = t.Label(self, text="Množství", font=fonts()["SMALL_FONT"])
-        self.quantity_label.grid(row=1, column=0, sticky="nswe", pady=10)
+        self.quantity_label.grid(row=0, column=0, sticky="nswe", pady=10)
         self.dispersion_label = t.Label(self, text="Rozptyl", font=fonts()["SMALL_FONT"])
-        self.dispersion_label.grid(row=1, column=1, sticky="nswe", pady=10)
+        self.dispersion_label.grid(row=0, column=1, sticky="nswe", pady=10)
 
         self.quantity = Scale(self, activebackground="aqua", bd=0, from_=1, to=MAX_NOISE_QUANTITY, orient=HORIZONTAL,
                               variable=self.quantity_variable)
-        self.quantity.grid(row=2, column=0, sticky="we")
+        self.quantity.grid(row=1, column=0, sticky="we")
         self.quantity.bind("<ButtonRelease-1>",
                            lambda event: self.__do_live_update())
 
         self.dispersion = Scale(self, activebackground="aqua", bd=0, from_=1, to=MAX_NOISE_DISPERSION, orient=HORIZONTAL,
                                 variable=self.dispersion_variable)
-        self.dispersion.grid(row=2, column=1, sticky="we")
+        self.dispersion.grid(row=1, column=1, sticky="we")
         self.dispersion.bind("<ButtonRelease-1>", lambda event: self.__do_live_update())
 
         self.color = BaseColorPicker(self, special_command=self.noise_update_color)
         self.color.bind("<ButtonRelease-1>",lambda event: self.__do_live_update())
-        self.color.grid(row=3, column=0, columnspan=2, sticky="we", pady=10)
+        self.color.grid(row=2, column=0, columnspan=2, sticky="we", pady=10)
 
         # LIMITS SETTINGS
         # self.limits_settings = LimitsSettings(parent=self, controller=controller)
@@ -64,7 +64,7 @@ class Noise(Frame):
 
         # GRID SETTINGS
         self.grid_settings = GridSettings(parent=self, controller=controller)
-        self.grid_settings.place(bordermode=OUTSIDE, x=MAX_WIDTH * .001, y=MAX_HEIGHT * .15,
+        self.grid_settings.place(bordermode=OUTSIDE, x=MAX_WIDTH * .001, y=MAX_HEIGHT * .17,
                                    width=MAX_WIDTH * .18,
                                    height=MAX_HEIGHT * .3)
 
